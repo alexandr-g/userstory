c42290afd85ac24314325e09909950d14b832e74
Alexandr <alexgrischuk@gmail.com>
2015-06-13 23:29:44 +0300

    Add login API

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,8 +1,22 @@
 var User = require('../models/user');
 var config = require('../../config');
+var jsonwebtoken = require('jsonwebtoken');
 
 var secretKey = config.secretKey;
 
+function createToken(user) {
+
+	var token = jsonwebtoken.sign({
+		_id: user._id,
+		name: user.name,
+		username: user.username
+	}, secretKey, {
+		expirtesInMinute: 1440
+	});
+
+	return token;
+
+}
 
 module.exports = function(app, express) {
 
@@ -31,16 +45,47 @@ module.exports = function(app, express) {
 
 	api.get('/users', function(req, res) {
 
-		User.find({},function(err, users) {
+		User.find({}, function(err, users) {
 			if(err) {
 				res.send(err);
 				return;
 			}
 
 			res.json(users);
-			
+
 		});
 
+	});
+
+	api.post('/login', function(req, res) {
+
+		User.findOne({
+			username: req.body.username
+		}).select('password').exec(function(err, user) {
+
+			if(err) throw err;
+
+			if(!user) {
+				res.send({ message: 'User does not exist' });
+			} else if(user) {
+
+				var validPassword = user.comparePassword(req.body.password);
+
+				if(!validPassword) {
+					res.send({ message: 'Invalid password' });
+				} else {
+
+					//set token
+					var token = createToken(user);
+					
+					res.json({
+						success: true,
+						message: "Successfuly login!",
+						token: token
+					});
+				}
+			}
+		});
 	});
 
 	return api;
