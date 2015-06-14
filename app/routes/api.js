eb7fcad0b46b0e89fd77882d10757ffa2f260e66
Alexandr <alexgrischuk@gmail.com>
2015-06-14 11:22:17 +0300

    Create custom middleware

--- app/routes/api.js
+++ app/routes/api.js
@@ -88,6 +88,37 @@ module.exports = function(app, express) {
 		});
 	});
 
+	api.use(function(req, res, next) {
+
+		console.log("Somebody just came to our app!");
+
+		var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+		//check if token exist
+		if (token) {
+
+			jsonwebtoken.verify(token, secretKey, function(err, decoded) {
+
+				if (err) {
+					res.status(403).send({ success: false, message: "Failed to authenticate user" });
+
+				} else {
+
+					req.decoded = decoded;
+					next();
+				}
+			});
+		} else {
+			res.status(403).send({ success: false, message: "No token provided" });
+		}
+
+	});
+
+	api.get('/', function(req, res) {
+
+		res.json("hello bastard!");
+	});
+
 	return api;
 
 }
